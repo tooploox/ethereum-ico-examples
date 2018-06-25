05173001a77e60b587999e50a147bb91f79a6576
Mariusz Żak <mariuszzak21@gmail.com>
2018-06-25 13:42:42 +0200

    Add GenericCrowdsale to the migration

--- migrations/2_generic-crowdsale.js
+++ migrations/2_generic-crowdsale.js
@@ -1,5 +1,12 @@
-const SimpleToken = artifacts.require("./02-presale-multiple-rounds/SimpleToken.sol");
+const SimpleToken = artifacts.require("./SimpleToken.sol");
+const GenericCrowdsale = artifacts.require("./01-generic/GenericCrowdsale.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleToken, "Tooploox", "TPX", 18, 21000000);
+module.exports = async (deployer, network, [owner]) => {
+  await deployer.deploy(SimpleToken, "Tooploox", "TPX", 18, 21000000);
+
+  const openingTime = Date.now() + 100000;
+  const closingTime = Date.now() + 200000;
+  const rate = 100;
+
+  deployer.deploy(GenericCrowdsale, openingTime, closingTime, rate, owner, SimpleToken.address);
 };
