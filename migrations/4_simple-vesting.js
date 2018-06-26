89169f05179476889cb560b0dc2cf675e93753cb
Mariusz Żak <mariuszzak21@gmail.com>
2018-06-26 15:51:11 +0200

    Set options for each beneficiary

--- migrations/4_simple-vesting.js
+++ migrations/4_simple-vesting.js
@@ -2,17 +2,29 @@ const SimpleTokenVesting = artifacts.require("./03-token-vesting/SimpleTokenVest
 const SimpleToken = artifacts.require("./SimpleToken.sol");
 
 module.exports = (deployer, network, [owner]) => {
-  deployTokenVestingContracts(deployer);
+  const beneficiaries = [
+    {
+      name:    'developer1',
+      address: 0x7319879Eb04477e39c2dbaA846470FEb7b2966e1,
+      options: 15,
+    },
+    {
+      name:    'developer2',
+      address: 0x9e9Bf5E5D100f45a3527c7eacC84E0a185fc7E0D,
+      options: 15,
+    },
+    {
+      name:    'crowdsale',
+      address: 0x7319879Eb04477e39c2dbaA846470FEb7b2966e1,
+      options: 70
+    }
+  ];
+
+  deployTokenVestingContracts(deployer, beneficiaries);
   deployToken(deployer);
 };
 
-function deployTokenVestingContracts(deployer) {
-  const beneficiaries = [
-    0x7319879Eb04477e39c2dbaA846470FEb7b2966e1,
-    0xa2164D11832e1De722c24ed87C82c53318f23b87,
-    0x9e9Bf5E5D100f45a3527c7eacC84E0a185fc7E0D
-  ];
-
+function deployTokenVestingContracts(deployer, beneficiaries) {
   const now = Date.now() / 1000;
   const day = 24 * 60 * 60;
   const start = now + day;
@@ -22,7 +34,7 @@ function deployTokenVestingContracts(deployer) {
 
   beneficiaries.forEach((beneficiary) =>
   {
-    deployer.deploy(SimpleTokenVesting, beneficiary, start, cliff, duration, revocable);
+    deployer.deploy(SimpleTokenVesting, beneficiary.address, start, cliff, duration, revocable);
   });
 }
 
