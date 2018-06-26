10d3e2f141b2c0a64bad0b6cfbc203922273a062
Mariusz Żak <mariuszzak21@gmail.com>
2018-06-26 15:26:57 +0200

    Deploy a TokenVesting contract per a beneficiary

--- migrations/4_simple-vesting.js
+++ migrations/4_simple-vesting.js
@@ -1,7 +1,11 @@
 const SimpleTokenVesting = artifacts.require("./03-token-vesting/SimpleTokenVesting.sol");
 
-module.exports = async (deployer, network, [owner]) => {
-  const beneficiary = 0x7319879Eb04477e39c2dbaA846470FEb7b2966e1;
+module.exports = (deployer, network, [owner]) => {
+  const beneficiaries = [
+    0x7319879Eb04477e39c2dbaA846470FEb7b2966e1,
+    0xa2164D11832e1De722c24ed87C82c53318f23b87,
+    0x9e9Bf5E5D100f45a3527c7eacC84E0a185fc7E0D
+  ];
 
   const now = Date.now() / 1000;
   const day = 24 * 60 * 60;
@@ -10,5 +14,8 @@ module.exports = async (deployer, network, [owner]) => {
   const duration = 3 * 365 * day;
   const revocable = false;
 
-  await deployer.deploy(SimpleTokenVesting, beneficiary, start, cliff, duration, revocable);
+  beneficiaries.forEach((beneficiary) =>
+  {
+    deployer.deploy(SimpleTokenVesting, beneficiary, start, cliff, duration, revocable);
+  });
 };
