fc6ae19e84e6aef71176da4020b81b4a7b426291
Michal Zalecki <michal@michalzalecki.com>
2018-06-29 19:32:34 +0200

    Improve migration scripts

--- migrations/2_generic-crowdsale.js
+++ migrations/2_generic-crowdsale.js
@@ -4,12 +4,16 @@ const GenericCrowdsale = artifacts.require("./01-generic/GenericCrowdsale.sol");
 module.exports = async (deployer, network, [owner]) => {
   await deployer.deploy(SimpleToken, "Tooploox", "TPX", 18, 21000000);
 
-  const now = Date.now() / 1000;
+  const now = Math.floor(Date.now() / 1000);
   const day = 24 * 60 * 60;
 
-  const openingTime = now + day;
+  const openingTime = now;
   const closingTime = now + 2 * day;
-  const rate = 100;
+  const rate = 1000;
 
-  deployer.deploy(GenericCrowdsale, openingTime, closingTime, rate, owner, SimpleToken.address);
+  await deployer.deploy(GenericCrowdsale, openingTime, closingTime, rate, owner, SimpleToken.address);
+
+  console.log(`Transfering tokens...`);
+  const Token = await SimpleToken.deployed();
+  await Token.transfer(GenericCrowdsale.address, 20000000 * 10 ** 18, { from: owner });
 };
--- migrations/3_presale-multiple-rounds.js
+++ migrations/3_presale-multiple-rounds.js
@@ -2,6 +2,7 @@ const SimpleToken = artifacts.require("./SimpleToken.sol");
 const PresaleCrowdsale = artifacts.require("./02-presale-multiple-rounds/PresaleCrowdsale.sol");
 
 module.exports = async (deployer, network, [owner]) => {
-  await deployer.deploy(SimpleToken, "Tooploox", "TPX", 18, 21000000);
+  // SimpleToken already deployed by the 2nd migration
+  // await deployer.deploy(SimpleToken, "Tooploox", "TPX", 18, 21000000);
   deployer.deploy(PresaleCrowdsale, 10000, owner, SimpleToken.address, owner);
 };
